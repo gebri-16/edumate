5f1e5a13c4d7c6e2a85210904fc333bd3ccfbb45
Bicanro Panjaitan <bicanropanjaitan@gmail.com>
2026-03-22 22:05:30 +0700

    serve static frontend

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ require("./scheduler");
 // Middleware
 app.use(cors());
 app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/pages/login.html'));
+});
 app.use(express.urlencoded({ extended: true }));
 
 // Session - dibutuhkan untuk Google OAuth
